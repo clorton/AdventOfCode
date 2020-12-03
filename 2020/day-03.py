9123025f5a2f303c0a049a93446664f11c993e24
Christopher Lorton <github@lortons.net>
2020-12-02 22:37:35 -0800

    AoC 2020 Day 03

---
+++ 2020/day-03.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python3
+
+from functools import reduce
+from pathlib import Path
+
+text = (Path(__file__).parent.absolute() / "day-03.txt").read_text()
+
+# part 1
+lines = text.split("\n")
+
+
+def trees_for_slope(dx: int, dy: int, lines: list ) -> int:
+    x = 0
+    y = 0
+    # dx = 3
+    # dy = 1
+    count = 0
+    while y < len(lines):
+        if lines[y][x] == '#':
+            count += 1
+        x += dx
+        x %= len(lines[0])
+        y += dy
+
+    return count
+
+count = trees_for_slope(3, 1, lines)
+
+print(f"Encountered {count} trees.")
+
+slopes = [
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2)
+]
+
+counts = []
+for dx, dy in slopes:
+    counts.append(trees_for_slope(dx, dy, lines))
+
+print(f"Encountered {counts} trees.")
+product = reduce(lambda x, y: x * y, counts, 1)
+print(f"Product is {product}.")
