861b6123a684b7a405308d737ddf20ee11e5c0c7
Christopher Lorton <github@lortons.net>
2022-12-12 20:15:35 +0000

    2017 Day 10 WIP

--- 2017/2017-10.py
+++ 2017/2017-10.py
@@ -1,49 +1,62 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 from functools import reduce
+from typing import List
 
-with open('2017-10.txt', 'r') as handle:
+import numpy as np
+
+from pathlib import Path
+WORK_DIR = Path(__file__).parent.absolute()
+
+with (WORK_DIR / "2017-10.txt").open("r") as handle:
     line = handle.readline()
 
 
-def contents(source, index, count):
+def contents(source: List, index: int, count: int) -> List:
     return [source[i % len(source)] for i in range(index, index+count)]
 
 
-def update(destination, index, count, source):
+def update(destination: List, index: int, count: int, source: List) -> None:
     for i in range(count):
         destination[(index+i) % len(destination)] = source[i]
+    return
 
 
-lengths = [int(entry) for entry in line.split(',')]
-memory = [i for i in range(256)]
-current_position = 0
-skip_size = 0
-for length in lengths:
-    extract = contents(memory, current_position, length)
-    reverse = [item for item in reversed(extract)]
-    update(memory, current_position, length, reverse)
-    current_position += length + skip_size
-    skip_size += 1
-
-product = memory[0] * memory[1]
-print('memory[0] = {0}, memory[1] = {1}, product = {2}'.format(memory[0], memory[1], product))
-
-
-lengths = [int(b) for b in bytes(line, 'utf-8')]
-lengths.extend([17,31,73,47,23])
-memory = [i for i in range(256)]
-current_position = 0
-skip_size = 0
-for round in range(64):
+def main() -> None:
+    lengths = [int(entry) for entry in line.split(',')]
+    memory = np.arange(256, dtype=np.int32) # list(range(256))
+    current_position = 0
+    skip_size = 0
     for length in lengths:
         extract = contents(memory, current_position, length)
-        reverse = [item for item in reversed(extract)]
+        reverse = np.array(list(reversed(extract)), dtype=np.int32)   # list(reversed(extract))
         update(memory, current_position, length, reverse)
         current_position += length + skip_size
         skip_size += 1
 
-hash = [reduce(lambda x, y: x ^ y, memory[start:start+16]) for start in range(0, 256, 16)]
-string = reduce(lambda x, y: x + '{0:02x}'.format(y), hash, '')
-print('Hash = {0}'.format(hash))
-print('Hex  = {0}'.format(string))
+    product = memory[0] * memory[1]
+    print(f'memory[0] = {memory[0]}, memory[1] = {memory[1]}, product = {product}')
+
+    lengths = [int(b) for b in bytes(line, 'utf-8')]
+    lengths.extend([17,31,73,47,23])
+    memory = np.arange(256, dtype=np.int32) # list(range(256))
+    current_position = 0
+    skip_size = 0
+    for _ in range(64):
+        for length in lengths:
+            extract = contents(memory, current_position, length)
+            reverse = np.array(list(reversed(extract)), dtype=np.int32)
+            update(memory, current_position, length, reverse)
+            current_position += length + skip_size
+            skip_size += 1
+
+    hashstring = [reduce(lambda x, y: x ^ y, memory[start:start+16]) for start in range(0, 256, 16)]
+    string = reduce(lambda x, y: x + f'{y:02x}', hashstring, '')
+    print(f'Hash = {hashstring}')
+    print(f'Hex  = {string}')
+
+    return
+
+
+if __name__ == "__main__":
+    main()
