6b06d8b72368a555a70bcea552090b110fde6e12
Christopher Lorton <github@lortons.net>
2022-12-20 19:37:39 +0000

    2022 Day 20 WIP Passes part 1 on test and actual data. Appears to pass part 2 on test data, but is coincidental.

--- 2022/2022-20.py
+++ 2022/2022-20.py
@@ -15,59 +15,131 @@ test = [1,2,-3,3,-2,0,4]
 
 print(inputs[0:8])
 
-state = np.array(inputs,dtype=np.int32)
-positions = {value:index for index,value in enumerate(state)}
+VALUE = 0
+NEXT = 1
+PREV = 2
+length = len(inputs)
+linked = np.zeros((length, 3), dtype=np.int32)
 
-print(state[0:8])
-# print(positions)
-
-tstart = datetime.now()
 for index, value in enumerate(inputs):
+    linked[index, VALUE] = value
+    linked[index, NEXT] = (index+1)%length
+    linked[index, PREV] = (index-1)%length
 
-    current = positions[value]
-    new = current + value
-    if new >= len(state):
-        new = new % len(state) + 1
-    elif new < 0:
-        new = new % len(state) - 1
-    delta = abs(new - current)
+def unlink(index, linkedlist):
 
-    if index < 8:
-        print(f"{state} {value=:2} {current=:1} new0={(current+value):2} {new=:1} ", end="")
+    linkedlist[linkedlist[index, NEXT], PREV] = linkedlist[index, PREV]
+    linkedlist[linkedlist[index, PREV], NEXT] = linkedlist[index, NEXT]
 
-    if new > current:
+    return
 
-        first = current
-        last = first + delta - 1
+def insert_after(target, index, linkedlist):
 
-        state[first:last+1] = state[first+1:last+2]
-        state[new] = value
+    save_next = linkedlist[target, NEXT]
+    linkedlist[index, NEXT] = save_next
+    linkedlist[index, PREV] = target
+    linkedlist[target, NEXT] = index
+    linkedlist[save_next, PREV] = index
 
-        for i in range(current, new+1):
-            positions[state[i]] = i
+    return
 
-    elif new < current:
+def insert_before(target, index, linkedlist):
 
-        first = new + 1
-        last = first + delta - 1
+    save_prev = linkedlist[target, PREV]
+    linkedlist[index, NEXT] = target
+    linkedlist[index, PREV] = save_prev
+    linkedlist[target, PREV] = index
+    linkedlist[save_prev, NEXT] = index
 
-        state[first:last+1] = state[first-1:last]
-        state[new] = value
+    return
 
-        for i in range(new, current+1):
-            positions[state[i]] = i
+def mix(inputs, linkedlist, iteration="?"):
 
-    else:
-        pass    # nothing changes (value is 0)
+    tstart = datetime.now()
+    for index, value in enumerate(inputs):
 
-    if index < 8:
-        print(f"{state}")
-tend = datetime.now()
+        if value == 0:
+            continue
 
-izero = positions[0]
-einsk = state[(izero+1000)%len(state)]
-zweik = state[(izero+2000)%len(state)]
-dreik = state[(izero+3000)%len(state)]
+        skip = value
 
-# Answer != -13132 (-2622 + -8866 + -1624)
-print(f"{tend-tstart}: {einsk=}, {zweik=}, {dreik=} ... {einsk+zweik+dreik}")
+        unlink(index, linkedlist)
+        current = index
+
+        if skip > 0:
+
+            current = linkedlist[current, NEXT]
+            skip -= 1
+
+            skip = (skip % (length-1))
+
+            for _ in range(skip):
+                current = linkedlist[current, NEXT]
+
+            insert_after(current, index, linkedlist)
+
+        else:   # skip < 0:
+
+            current = linkedlist[current, PREV]
+            skip += 1
+
+            skip = (abs(skip) % (length-1))
+
+            for _ in range(abs(skip)):
+                current = linkedlist[current, PREV]
+
+            insert_before(current, index, linkedlist)
+
+    tend = datetime.now()
+
+    if length <= 16:
+        print(f"{iteration}: ", end="")
+        current = 0
+        for _ in range(length):
+            print(f"{linkedlist[current, VALUE]} ", end="")
+            current = linkedlist[current, NEXT]
+        print()
+
+    return tend-tstart
+
+elapsed = mix(inputs, linked)
+
+def get_coordinates(linkedlist, elapsed):
+
+    izero = np.nonzero(linkedlist[:,VALUE] == 0)[0][0]
+
+    current = izero
+    for _ in range(1000):
+        current = linkedlist[current, NEXT]
+    einsk = linkedlist[current, VALUE]
+    for _ in range(1000):
+        current = linkedlist[current, NEXT]
+    zweik = linkedlist[current, VALUE]
+    for _ in range(1000):
+        current = linkedlist[current, NEXT]
+    dreik = linkedlist[current, VALUE]
+
+    print(f"{elapsed}: {einsk=}, {zweik=}, {dreik=} ... {einsk+zweik+dreik}")
+
+    return
+
+get_coordinates(linked, elapsed)
+
+KEY = 811589153
+decrypted = np.zeros((length, 3), dtype=np.int64)
+
+for index, value in enumerate(inputs):
+    decrypted[index, VALUE] = value*KEY
+    decrypted[index, NEXT] = (index+1)%length
+    decrypted[index, PREV] = (index-1)%length
+
+if length <= 16:
+    print(" ".join(map(str, decrypted[:,VALUE])))
+
+elapsed = datetime.now()
+elapsed -= elapsed
+for _ in range(10):
+    elapsed += mix(inputs, decrypted, _)
+
+# 5449009573242 is too high...
+get_coordinates(decrypted, elapsed)
