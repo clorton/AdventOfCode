f35e36a88d237bca8221f5ed7d782bc694a29ade
Christopher Lorton <github@lortons.net>
2024-12-14 19:09:06 +0000

    2024 Day 14

--- 2024/day-14.py
+++ 2024/day-14.py
@@ -1,27 +1,52 @@
 from pathlib import Path
-# import re
+import re
 
-# import numpy as np
+import numpy as np
 
 with (Path(__file__).parent / "day-14.txt").open("rt") as file:
     lines = [line.strip() for line in file.readlines()]
 
+width = 101
+height = 103
+
 # lines = [
-#     "Button A: X+94, Y+34",
-#     "Button B: X+22, Y+67",
-#     "Prize: X=8400, Y=5400",
-#     "",
-#     "Button A: X+26, Y+66",
-#     "Button B: X+67, Y+21",
-#     "Prize: X=12748, Y=12176",
-#     "",
-#     "Button A: X+17, Y+86",
-#     "Button B: X+84, Y+37",
-#     "Prize: X=7870, Y=6450",
-#     "",
-#     "Button A: X+69, Y+23",
-#     "Button B: X+27, Y+71",
-#     "Prize: X=18641, Y=10279",
+#     "p=0,4 v=3,-3",
+#     "p=6,3 v=-1,-3",
+#     "p=10,3 v=-1,2",
+#     "p=2,0 v=2,-1",
+#     "p=0,0 v=1,3",
+#     "p=3,0 v=-2,-2",
+#     "p=7,6 v=-1,-3",
+#     "p=3,0 v=-1,-2",
+#     "p=9,3 v=2,3",
+#     "p=7,3 v=-1,2",
+#     "p=2,4 v=2,-3",
+#     "p=9,5 v=-3,-3",
 # ]
+
+# width = 11
+# height = 7
+
+from collections import namedtuple
+Robot = namedtuple("Robot", ["x0", "y0", "vx", "vy"])
+
+robots = []
+
+for line in lines:
+    m = re.match(r"p=([0-9]+),([0-9]+) v=(-?[0-9]+),(-?[0-9]+)", line)
+    robots.append(Robot(int(m[1]), int(m[2]), int(m[3]), int(m[4])))
+
+floor = np.zeros((height, width), dtype=np.int32)
+for robot in robots:
+    x = (robot.x0 + 100 * robot.vx) % width
+    y = (robot.y0 + 100 * robot.vy) % height
+    floor[y, x] += 1
+
+nw = floor[:height//2, :width//2].sum()
+ne = floor[:height//2, width//2+1:].sum()
+sw = floor[height//2+1:, :width//2].sum()
+se = floor[height//2+1:, width//2+1:].sum()
+
+print(f"Safety factor = {nw} * {ne} * {sw} * {se} = {nw*ne*sw*se}")
 
 print("Done.")
---
+++ 2024/day-15.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+# import re
+
+# import numpy as np
+
+with (Path(__file__).parent / "day-15.txt").open("rt") as file:
+    lines = [line.strip() for line in file.readlines()]
+
+# lines = [
+#     "p=0,4 v=3,-3",
+#     "p=6,3 v=-1,-3",
+#     "p=10,3 v=-1,2",
+#     "p=2,0 v=2,-1",
+#     "p=0,0 v=1,3",
+#     "p=3,0 v=-2,-2",
+#     "p=7,6 v=-1,-3",
+#     "p=3,0 v=-1,-2",
+#     "p=9,3 v=2,3",
+#     "p=7,3 v=-1,2",
+#     "p=2,4 v=2,-3",
+#     "p=9,5 v=-3,-3",
+# ]
+
+print("Done.")
