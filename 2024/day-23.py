d3ee453d7fbbda13d2caf26b49d5a9b10f32ab46
Christopher Lorton <github@lortons.net>
2024-12-23 05:28:01 +0000

    2024 Day 23

--- 2024/day-23.py
+++ 2024/day-23.py
@@ -6,8 +6,76 @@ from tqdm import tqdm
 with (Path(__file__).parent / "day-23.txt").open("rt") as file:
     lines = [line.strip() for line in file.readlines()]
 
-lines = [
-     "value"
-]
+# lines = [
+#     "kh-tc",
+#     "qp-kh",
+#     "de-cg",
+#     "ka-co",
+#     "yn-aq",
+#     "qp-ub",
+#     "cg-tb",
+#     "vc-aq",
+#     "tb-ka",
+#     "wh-tc",
+#     "yn-cg",
+#     "kh-ub",
+#     "ta-co",
+#     "de-co",
+#     "tc-td",
+#     "tb-wq",
+#     "wh-td",
+#     "ta-ka",
+#     "td-qp",
+#     "aq-cg",
+#     "wq-ub",
+#     "ub-vc",
+#     "de-ta",
+#     "wq-aq",
+#     "wq-vc",
+#     "wh-yn",
+#     "ka-de",
+#     "kh-ta",
+#     "co-tc",
+#     "wh-qp",
+#     "tb-vc",
+#     "td-yn",
+# ]
+
+from collections import defaultdict
+connections = defaultdict(set)
+
+for line in lines:
+    a, b = line.split("-")
+    connections[a].add(b)
+    connections[b].add(a)
+
+from itertools import combinations
+
+threes = defaultdict(set)
+for k, v in connections.items():
+    for b, c in combinations(v, 2):
+        threes[",".join(sorted([k, b, c]))].add(k)
+
+triples = {k:threes[k] for k in sorted(filter(lambda k: len(threes[k]) == 3, threes.keys()))}
+
+hasat = {k:v for k, v in triples.items() if any(filter(lambda e: e.startswith("t"), v))}
+
+print(f"len(hasat) = {len(hasat)}")
+
+# Part 2
+
+import networkx as nx
+
+# Create a sample graph
+G = nx.Graph()
+G.add_edges_from([(a, b) for a, b in map(lambda l: l.split("-"), lines)])
+
+# Find all cliques in the graph
+all_cliques = list(nx.find_cliques(G))
+
+# Find the largest clique
+largest_clique = max(all_cliques, key=len)
+
+print("Largest clique:", ",".join(sorted(largest_clique)))
 
 print("Done.")
---
+++ 2024/day-24.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+import numpy as np
+from tqdm import tqdm
+
+with (Path(__file__).parent / "day-24.txt").open("rt") as file:
+    lines = [line.strip() for line in file.readlines()]
+
+lines = [
+]
+
+print("Done.")
